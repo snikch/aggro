46142a8c5a8158275d032b969debb0f03990e963
Mal Curtis <mal@mal.co.nz>
2017-02-20 21:16:13 +1300

    Fix spelling

--- sort.go
+++ sort.go
@@ -9,8 +9,8 @@ type Sortable interface {
 	Less(a, b *ResultBucket) bool
 }
 
-// sortableForOptions returns the appropriate sorter for the supplied sort
-// options. If the options type provided is invalid, it is ignored an no sorter
+// sortableForOptions returns the sortable implementation for the supplied sort
+// options. If the options type provided is invalid, it is ignored and no sorter
 // is returned. A nil response here will end up with results being in a
 // non-deterministic order due to randomisation when ranging over maps.
 func sortableForOptions(options *SortOptions) Sortable {
@@ -25,17 +25,17 @@ func sortableForOptions(options *SortOptions) Sortable {
 	return nil
 }
 
-// AlphabeticalSortable is a simple  sorter that sorts alphabetically in the
+// AlphabeticalSortable is a simple sorter that sorts alphabetically in the
 // direction of the boolean, true meaning ascending and false being descending.
 type AlphabeticalSortable bool
 
-// Less implements Sortable by comparing the values field of each result.
+// Less implements Sortable by comparing the value field of each result.
 func (sortable *AlphabeticalSortable) Less(a, b *ResultBucket) bool {
 	return a.Value < b.Value == bool(*sortable)
 }
 
 // bucketSorter is an implementation of the sort.Sort interface that is capable
-// or sorting the supplied slice of results with the supplied Sortable.
+// of sorting the supplied slice of results with the supplied Sortable.
 type bucketSorter struct {
 	results  []*ResultBucket
 	sortable Sortable
@@ -51,7 +51,7 @@ func (sorter *bucketSorter) Swap(i, j int) {
 	sorter.results[i], sorter.results[j] = sorter.results[j], sorter.results[i]
 }
 
-// Less implement the sort.Sort less method by calling the Sortable that the
+// Less implements the sort.Sort Less method by calling the Sortable that the
 // sorter has been supplied, with the two appropriate results.
 func (sorter *bucketSorter) Less(i, j int) bool {
 	return sorter.sortable.Less(sorter.results[i], sorter.results[j])
